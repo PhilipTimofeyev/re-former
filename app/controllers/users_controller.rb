b8ee3c03e6ee6b6d9c63e5bdeb79e69117990c3e
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-08 09:39:11 -0700

    Change HTML to use #form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
 	def new
+		@user = User.new
 	end
 
 	def create
-		@user = User.new(user_params)
+		puts :query
+		# @user = User.new(user_params)
 
-		if @user.save
-			redirect_to new_user_path
-		else
-			render :new, status: :unprocessable_entity
-		end
+		# if @user.save
+		# 	redirect_to new_user_path
+		# else
+		# 	render :new, status: :unprocessable_entity
+		# end
 	end
 
 	private
